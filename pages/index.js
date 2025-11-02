396312140d759cc2650b7a649078aada09362343
joao marcelo <joaomarcelodev.contato@gmail.com>
2025-11-02 11:07:58 -0300

    adding new lint scripts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return(
-        <h1>Amandoquinha, te amo muito! Se voce ma ama de uma risadinha</h1>
-    )
+  return <h1>Amandoquinha, te amo muito! Se voce ma ama de uma risadinha</h1>;
 }
 
 export default Home;
