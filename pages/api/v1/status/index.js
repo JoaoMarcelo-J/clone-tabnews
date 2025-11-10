9aaa03a77c542478ea3bbcbcb342086ba0bb0290
joao marcelo <joaomarcelodev.contato@gmail.com>
2025-11-10 13:50:01 -0300

    jsconfig.json added

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  res.status(200).json({ status: "okll" });
+  res.status(200).json({ status: "ok" });
 }
 
 export default status;
