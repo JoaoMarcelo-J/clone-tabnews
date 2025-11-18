c286974e3b4d5eacee119db73c118b2a54b99bae
joao marcelo <joaomarcelodev.contato@gmail.com>
2025-11-17 21:26:09 -0300

    chore

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -39,5 +39,5 @@ export default async function migrations(req, res) {
     return res.status(200).json(migratedMigrations);
   }
 
-  return res.status(405).json({ error: "Method not allowed" });
+  return res.status(405).json({ error: "Method not allowedd" });
 }
