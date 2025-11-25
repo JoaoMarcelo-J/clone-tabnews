a49bf75f3a4b483fded67068a2ccaf8acaabb1b3
joao marcelo <joaomarcelodev.contato@gmail.com>
2025-11-24 22:34:26 -0300

    ci: add lint for secret keys

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 function Home() {
-  const aws_secret_access_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
-  console.log(aws_secret_access_key);
-
   return <h1>Amandoquinha, te amo muito! Se voce ma ama de uma risadinha</h1>;
 }
 
