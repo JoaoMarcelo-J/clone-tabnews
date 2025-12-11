e07043f5d18f47fe5504932a04d682bf32d6ebdc
joao marcelo <joaomarcelodev.contato@gmail.com>
2025-12-11 19:58:30 -0300

    feat: add password model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import database from "infra/database";
+import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors";
 
 async function findOneByUsername(username) {
@@ -34,6 +35,7 @@ async function findOneByUsername(username) {
 
 async function create(userInputValues) {
   await validateUniqueFields(userInputValues.username, userInputValues.email);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -74,6 +76,11 @@ async function create(userInputValues) {
         });
       }
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
