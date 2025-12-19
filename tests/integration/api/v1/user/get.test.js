eedd90073e8ee0b4e7dec162065eb8801cefcf99
joao marcelo <joaomarcelodev.contato@gmail.com>
2025-12-18 21:02:20 -0300

    feat: invalidate session cookie on unauthorized error

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -166,6 +166,19 @@ describe("GET /api/v1/user", () => {
         action: "Verifique se este usuário está logado e tente novamente",
         statusCode: 401,
       });
+
+      // Set-Cookie assertions
+      const parsedSetCookie = setCookieParser(response2, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
+      });
     });
 
     test("With expired session", async () => {
@@ -196,6 +209,19 @@ describe("GET /api/v1/user", () => {
         message: "Usuário não possui sessão ativa",
         action: "Verifique se este usuário está logado e tente novamente",
         statusCode: 401,
+      });
+
+      // Set-Cookie assertions
+      const parsedSetCookie = setCookieParser(response2, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
       });
     });
   });
